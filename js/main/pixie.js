a199ecaa92776d787d93cf77c8f8fd50e39196ab
Janne Kainulainen <janne.kainulainen@reaktor.fi>
2013-01-31 15:50:22 +0200

    add more testdata

--- js/main/pixie.js
+++ js/main/pixie.js
@@ -5,7 +5,8 @@ define(function (require) {
 
     var dummyChunk = JSON.stringify([
         'https://www.google.com/images/srpr/logo3w.png',
-        'https://www.google.com/intl/en_ALL/images/logos/images_logo_lg.gif'
+        'https://www.google.com/intl/en_ALL/images/logos/images_logo_lg.gif',
+        'https://ssl.gstatic.com/images/logos/google_logo_41.png'
     ]);
     var pictures = new Bacon.Bus()
     pictures.plug(Bacon.once(dummyChunk))
